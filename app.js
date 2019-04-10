445140c472a1b2caa61d65f84c400c8409fafeb4
Kolev <1596048@cegep-heritage.qc.ca>
2019-04-10 09:07:34 -0400

    Latest version

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
+
+const rootRouter = require("./routes/root");
+const usersRouter = require("./routes/users");
+const numbersRouter = require("./routes/numbers");
+
 const port = 9000;
 
-const usersRouter = require('./routes/users');
+app.set("view engine", "hbs");
 
 app.use(cookieParser());
+app.use("/inc", express.static(path.join(__dirname, "inc")));
 
 app.use((req, res, next) => {
 	if (req.cookies.vcount == undefined) {
@@ -19,35 +25,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/users', usersRouter);
-
-app.get("/inc", (req, res) => {
-	return res.send(path.join(__dirname, "inc"));
-});
-app.use("/inc", express.static(path.join(__dirname, "inc")));
-
-app.get("/hello", (req, res) => {
-	return res.send(req.vcount + "<br>hey there");
-});
-
-app.get("/numbers/:from-:to", (req, res) => {
-	let a = parseInt(req.params.from);
-	let b = parseInt(req.params.to);
-
-	let html = "";
-
-	if (a > b) {
-		let t = a;
-		a = b;
-		b = t;
-	}
-
-	for (let x = a; x <= b; x++) {
-		html += `<div>${x}</div>`;
-	}
-
-	res.send(html);
-});
+app.use("/", rootRouter);
+app.use("/users", usersRouter);
+app.use("/numbers", numbersRouter);
 
 app.listen(port, function() {
 	return console.log("Sample App " + port + "!");
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+//router.use('/settings', userSettingRouter);
 router.get("/:userId/:email", (req, res) => {
 	let uid = parseInt(req.params.userId);
 	let email = req.params.email;
